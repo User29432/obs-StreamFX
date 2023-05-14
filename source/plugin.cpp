d30a02db5d90566256d06820a442b97b4444b155
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2023-05-14 08:00:26 +0200

    code: Migrate filter::color_grade to new loader code

--- source/plugin.cpp
+++ source/plugin.cpp
@@ -173,9 +173,6 @@ MODULE_EXPORT bool obs_module_load(void)
 #ifdef ENABLE_FILTER_BLUR
 			streamfx::filter::blur::blur_factory::initialize();
 #endif
-#ifdef ENABLE_FILTER_COLOR_GRADE
-			streamfx::filter::color_grade::color_grade_factory::initialize();
-#endif
 #ifdef ENABLE_FILTER_DENOISING
 			streamfx::filter::denoising::denoising_factory::initialize();
 #endif
@@ -259,9 +256,6 @@ MODULE_EXPORT void obs_module_unload(void)
 #endif
 #ifdef ENABLE_FILTER_BLUR
 			streamfx::filter::blur::blur_factory::finalize();
-#endif
-#ifdef ENABLE_FILTER_COLOR_GRADE
-			streamfx::filter::color_grade::color_grade_factory::finalize();
 #endif
 #ifdef ENABLE_FILTER_DENOISING
 			streamfx::filter::denoising::denoising_factory::finalize();
